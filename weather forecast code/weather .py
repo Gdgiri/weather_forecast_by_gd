2101929378956a08d07f0ab29d1ff72162b39ecc
Giridharan.S <96366499+Gdgiri@users.noreply.github.com>
2024-04-16 20:30:09 +0530

    Update weather .py
    save

--- weather forecast code/weather .py
+++ weather forecast code/weather .py
@@ -27,12 +27,7 @@ def getWeather():
     result = obj.timezone_at(lng=location.longitude,lat=location.latitude)
 
         
-    timezone.config(text=result)
-    long_lat.config(text=f"{round(location.latitude,4)}°N,{round(location.longitude,4)}°E")
-    home=pytz.timezone(result)
-    local_time=datetime.now(home)
-    current_time=local_time.strftime("%I:%M %p")
-    clock.config(text=current_time)
+   
 
     #weather
     api="https://api.openweathermap.org/data/2.5/onecall?lat="+str(location.latitude)+"&lon="+str(location.longitude)+"&units=metric&exclude=hourly&appid=6208ab40b769ad92d6e81a946742c26b"
